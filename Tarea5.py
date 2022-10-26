5e187d90ecfedb687c579e85e18027999a2ba829
Mariana Ruiz <m.ruizg@uniandes.edu.co>
2022-10-25 23:10:03 -0500

    cargar_grafo completo

--- Tarea5.py
+++ Tarea5.py
@@ -1,1 +1,16 @@
+import sys
+
+def cargar_grafo()->list:
+    linea = sys.stdin.readline()
+    grafo = []
+    while linea != "":
+        lista = []
+        for n in linea.split("\t"):
+            lista.append(int(n))
+        grafo.append(lista)
+        linea = sys.stdin.readline()
+    return grafo
+
+print(cargar_grafo())
+
 
